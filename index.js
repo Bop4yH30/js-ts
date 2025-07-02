6fa0112aad1d1d5248df27600877c4880c375034
Evgeniy <tanyagrish1961@gmail.com>
2025-07-03 00:34:44 +0300

    FizzBuzz

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-// апишите программу, которая генерирует случайное число между -2 и +2 и выводит в консоль “положительное” или “отрицательное”, в зависимости от того, какое число было сгенерировано.
-//
-//     Если число равно нулю, то вывести null
-const num = Math.random();
-const min = -2;
-const max = 2;
-const random = Math.floor(num * (max - min + 1) + min)
-if (random<0) {
-    console.log(`${random} отрицательное`);
-} else if (random>0) {
-    console.log(`${random} положительное`);
-} else if (random===0) {
-    console.log('null')
+//FizzBuzz
+const max = 20;
+const min = 1;
+const random = Math.floor(Math.random()*(max-min+1)+min);
+if (random%3===0) {
+    console.log(`Fizz`);
+} else if (random%5===0) {
+    console.log(`Buzz`);
+} else if (random%5===0 && random%3===0) {
+    console.log(`FizzBuzz`);
+} else {
+    console.log(`${random} не подходит поl условие`);
 }
+
 
