e1b3a1de065ee7d00cf7a1e5e25ef3ae6cc41b27
Evgeniy <tanyagrish1961@gmail.com>
2025-07-02 23:41:28 +0300

    Положительное или нет

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-
-
+// апишите программу, которая генерирует случайное число между -2 и +2 и выводит в консоль “положительное” или “отрицательное”, в зависимости от того, какое число было сгенерировано.
+//
+//     Если число равно нулю, то вывести null
+const num = Math.random();
+const min = -2;
+const max = 2;
+const random = Math.floor(num * (max - min + 1) + min)
+if (random<0) {
+    console.log(`${random} отрицательное`);
+} else if (random>0) {
+    console.log(`${random} положительное`);
+} else if (random===0) {
+    console.log('null')
+}
 
