88284f2c4ad13dc26ba38306b9499841e389e828
Evgeniy <tanyagrish1961@gmail.com>
2025-07-05 10:23:24 +0300

    Базовые операции

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 //Базовые операции объекты
 const person = {age:36, male:'men'};
-const wife = {age:20,male: 'women', inv:person }
+const wife = {age:20,male: 'women', inv:person };
 person.nick = 'Bop4yH';
 person['new-status'] = 'Big-Boss';
-wife.inv.family = 'married'
+wife.inv.family = 'married';
 const name ='Evgeniy';
 const age = 30;
 const child = 'Alex';
 const child2 = 'Chris'
 const men = {name,age,child};
 delete person.nick;
-person.child = child2
-console.log(person, wife)
-console.log (men)
+person.child = child2;
+console.log(person, wife);
+console.log (men);
