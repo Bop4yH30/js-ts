0df81003c55c6fc39dd236ce464b63c659b6f948
Evgeniy <tanyagrish1961@gmail.com>
2025-07-05 10:22:28 +0300

    Mapper

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
-//Маппер
-
-    const our = "accepted";
-let outer;
-
-switch (our) {
-    case 'approved':
-        outer = `approved`;
-        break;
-    case 'pending':
-    case 'moderation':
-    case 'waiting':
-            outer = `pending`;
-        break;
-    case 'declined':
-    case 'rejected':
-    case 'notaccepted':
-        outer = `declined`;
-        break;
-    default:
-        outer = 'pending';
-        break;
-}
-console.log(outer)
-
-
+//Базовые операции объекты
+const person = {age:36, male:'men'};
+const wife = {age:20,male: 'women', inv:person }
+person.nick = 'Bop4yH';
+person['new-status'] = 'Big-Boss';
+wife.inv.family = 'married'
+const name ='Evgeniy';
+const age = 30;
+const child = 'Alex';
+const child2 = 'Chris'
+const men = {name,age,child};
+delete person.nick;
+person.child = child2
+console.log(person, wife)
+console.log (men)
