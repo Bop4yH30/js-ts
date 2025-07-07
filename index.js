9aae8283c3fed7078e48caa91ff2651909692e74
Evgeniy <tanyagrish1961@gmail.com>
2025-07-07 19:55:32 +0300

    Подсчет

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-//Массивы соединитель
-const array = [1, 33, -10, 'example', 'word'];
-const str = array.join('_');
-console.log(str);
+// Циклы
+const array = [1, -3, 5, 6, 7, -5, -36, 4, 10, -7, 0];
+const count = array.length;
+const array1 = array.splice(array.length-2,1);
+let num1 = 0;
+
+ let positive = 1;
+ let negative = 1;
+
+while (count>num1 ) {
+    num1++;
+    if (array[num1] > 0) {
+        let po = positive++;
+    } else if (array[num1] < 0) {
+        let n = negative++;
+    }
+}
+console.log('Положительных:', positive);
+console.log('Отрицательных:', negative);
