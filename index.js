69beeccaf4eb68044549036df1d970a67c7a049c
Evgeniy <tanyagrish1961@gmail.com>
2025-07-17 14:08:26 +0300

    Ёлочка

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-//Циклы Округлить значения
-const num = [1.1 , 2.2, -2, 3.3, -1, 5.5, 5.99, 7.49, 7.9999, -2.2, -3]
-const num1 = [];
-let count = 0;
-for (let i = num.length-1; i >=0; i--) {
-    let m = num[i];
-    if (m<0 ) {
-        num1.unshift(num[i]);
-    } else if(m>0 && m>count && count<3) {
-        num1.unshift(num[i])
-        count++;
+//Циклы Ёлочка
+let h = 6;
+let tree =' ';
+let ornament = '*';
+for (let i=0; i<h; i++) {
+    let space = h-i-1;
+    let colSymbols = 2*i +1;
+    let line=tree.repeat(space) + ornament.repeat(colSymbols);
+     console.log(line);
     }
-        else if (m>0 && count>=3){
-            num1.unshift(Math.round(num[i]));
-        }
-    }
-console.log(num1);
+// console.log(tree)
