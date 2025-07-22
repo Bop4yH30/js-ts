cd0105ff1f3f34690958b6fe7e5b38dd8637637e
Evgeniy <tanyagrish1961@gmail.com>
2025-07-22 14:27:49 +0300

    Вывод Матрицы

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-//Циклы Ёлочка
-let h = 6;
-let tree =' ';
-let ornament = '*';
-for (let i=0; i<h; i++) {
-    let space = h-i-1;
-    let colSymbols = 2*i +1;
-    let line=tree.repeat(space) + ornament.repeat(colSymbols);
-     console.log(line);
-    }
-// console.log(tree)
+//Циклы Вывод матрицы
+const matrixs = [
+    ['Id', 'Title', 'Description'],
+    [1, 'Title-1', 'Description-1'],
+    [2, 'Title-2', 'Description-2'],
+    [3, 'Title-3', 'Description-3', 'Поле с багом'],
+    [4, 'Title-4', 'Description-4'],
+    [5, 'Title-5', 'Description-5'],
+    [6, 'Title-6', 'Description-6', 'Поле с багом'],
+];
+ let result='';
+ let arr = [];
+for (let i = 0; i < matrixs.length; i++) {
+         arr = matrixs[i];
+         result = '';
+      for (let j = 0; j <arr.length ; j++){
+          if (j===0){
+              result = `${result+' '+arr[j]}\t`;
+          }else {
+           result = `${result+' '+arr[j]}`;}
+      }
+      console.log(result);
+}
+
+        
+
+    
+
