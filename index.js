977dd6e3b813d146e80511b22f5280bcc88e512d
Evgeniy <tanyagrish1961@gmail.com>
2025-07-26 14:27:11 +0300

    Деление яблок

--- index.js
+++ index.js
@@ -1,29 +1,41 @@
-//Циклы Возраст
-const users = [
-    { name: 'Alice', age: 25 },
-    { name: 'Bob', age: 30 },
-    { name: 'Charlie', age: 25 },
-    { name: 'Diana', age: 35 },
-    { name: 'Eve', age: 30 },
-    { name: 'Frank', age: 28 },
-    { name: 'Grace', age: 25 },
-    { name: 'Hannah', age: 41 },
-    { name: 'Ivy', age: 30 },
-    { name: 'Jack', age: 28 }
-    ];
-const counter = {};
-for (const user of users) {
-    if (counter[user.age] === undefined) {
-        counter[user.age] = 0;
+//Циклы Деление яблок
+const mary = {
+    name: 'Mary',
+    wantApples: 2,
+};
+
+const alex = {
+    name: 'Alex',
+    wantApples: 1,
+};
+
+const mike = {
+    name: 'Mike',
+    wantApples: 5,
+};
+
+const brown = {
+    name: 'Brown',
+    wantApples: 4,
+};
+
+const people = [mary, alex, mike, brown];
+const apples = 11;
+
+const applesForOnePeople = Math.floor(apples/people.length);
+const happy = [];
+const notHappy = [];
+for (const person of people) {
+    person.eat = applesForOnePeople;
+    person.inHappy = applesForOnePeople >= person.wantApples;
+    if (person.inHappy){
+        happy.push(person)
+    }else {
+        notHappy.push(person);
     }
-
-    counter[user.age]++;
 }
-
-for (const key in counter) {
-    console.log(`Возраст=${key}. Количество людей=${counter[key]}`);
-}
-
+console.log(happy)
+console.log(notHappy)
         
 
     
